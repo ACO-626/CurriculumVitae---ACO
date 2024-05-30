36cefa6a1cb51dcd8dedba70765c357ae1ca8c3b
ACO-626 <ocampoalejandro.oc@gmail.com>
2024-05-29 18:43:15 -0600

    Updated May 2024

--- js/Saludo.js
+++ js/Saludo.js
@@ -4,17 +4,17 @@ function Fecha(){
 	
 
 	if ( hora <= 6) {
-		var_saludo="¡Hola de madrugada!";
+		var_saludo="¡Hola de madrugada! - Good early morning.!";
 
 
 	}else if ( hora <= 12){
-		var_saludo="¡Buenos días!";
+		var_saludo="¡Buenos días! - Good morning!";
 
 	}else if ( hora <= 19){
-		var_saludo="¡Buenas tardes!"
+		var_saludo="¡Buenas tardes! - Good afternoon!"
 	}
 	else{
-		var_saludo ="¡Buenas noches!";
+		var_saludo ="¡Buenas noches! - Good night!";
 	
 	}
 
